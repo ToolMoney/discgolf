e08e3dd79e1b15fcb987d31c21fb5b045b011cf9
ToolMoney <119289623+ToolMoney@users.noreply.github.com>
2023-06-16 17:06:56 -0700

    finished implementing CRUD to db api

--- src/api/disc.js
+++ src/api/disc.js
@@ -6,9 +6,6 @@ export function getDiscs() {
 }
 
 export function addDisc(disc) {
-    let rand = Math.floor(Math.random() * 10000000);
-    disc.id = rand;
-    // discs.push(disc);
     return fetch('http://127.0.0.1:5000/discs', {
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -17,33 +14,26 @@ export function addDisc(disc) {
 }
 
 export function editDisc(freshDisc) {
-    for (let disc of discs) {
-        if (freshDisc.id === disc.id) {
-            Object.assign(disc, freshDisc);
-            break;
-        }
-    }
-    return Promise.resolve(freshDisc);
+    return fetch(`http://127.0.0.1:5000/discs/${freshDisc.id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(freshDisc),
+    }).then((response) => response.json())
 }
 
 export function deleteDisc(discToDelete) {
-    for (let disc of discs) {
-        if (discToDelete.id === disc.id) {
-            let index = discs.indexOf(disc);
-            discs.splice(index, 1);
-            break;
-        }
-    }
-    return Promise.resolve();
+    return fetch(`http://127.0.0.1:5000/discs/${discToDelete.id}`, {
+        method: "DELETE",
+    })
 }
 
 
 
-const discs = [
-    {id: 1, name: "Flamethrower", speed: 11.5, glide: 6, turn: -2, fade: 2, inBag: true}, 
-    {id: 2, name: "Destroyer", speed: 12, glide: 5, turn: -1, fade: 3, inBag: true}, 
-    {id: 3, name: "Sword", speed: 12, glide: 5, turn: -0.5, fade: 2, inBag: false}, 
-    {id: 4, name: "Hatchet", speed: 9, glide: 6, turn: -2, fade: 1, inBag: false}, 
-    {id: 5, name: "Harp", speed: 4, glide: 3, turn: 0, fade: 3, inBag: true}, 
-    {id: 6, name: "Sensei", speed: 3, glide: 3, turn: 0, fade: 1, inBag: true}
-]
+// const discs = [
+//     {id: 1, name: "Flamethrower", speed: 11.5, glide: 6, turn: -2, fade: 2, inBag: true}, 
+//     {id: 2, name: "Destroyer", speed: 12, glide: 5, turn: -1, fade: 3, inBag: true}, 
+//     {id: 3, name: "Sword", speed: 12, glide: 5, turn: -0.5, fade: 2, inBag: false}, 
+//     {id: 4, name: "Hatchet", speed: 9, glide: 6, turn: -2, fade: 1, inBag: false}, 
+//     {id: 5, name: "Harp", speed: 4, glide: 3, turn: 0, fade: 3, inBag: true}, 
+//     {id: 6, name: "Sensei", speed: 3, glide: 3, turn: 0, fade: 1, inBag: true}
+// ]
